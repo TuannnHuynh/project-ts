29dd791598301639f0f8bb0f9061a920f069cc14
Tuấn Huỳnh Anh <huynhanhtuan8547@gmail.com>
2024-10-25 22:02:23 +0700

    Add component ScrollIndicator

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import LightDarkMode from "./components/LightDarkMode/LightDarkMode";
 import LoadMore from "./components/LoadMore/LoadMore";
 import QRCodeGeneration from "./components/QRCodeGeneration/QRCodeGeneration";
 import RandomColor from "./components/RandomColor/RandomColor";
+import ScrollIndicator from "./components/ScrollIndicator/ScrollIndicator";
 import StarRating from "./components/StarRating/StarRating";
 import TreeView from "./components/TreeView/TreeView";
 import menus from "./components/TreeView/data";
@@ -19,6 +20,7 @@ const App = () => {
       <TreeView menus={menus} />
       <QRCodeGeneration />
       <LightDarkMode />
+      <ScrollIndicator url={"https://dummyjson.com/products?limit=100"} />
     </div>
   );
 };
---
+++ src/components/ScrollIndicator/ScrollIndicator.tsx
@@ -0,0 +1,87 @@
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+
+type ScrollIndicatorProps = {
+  url: string; // Type the url prop
+};
+
+type TProducts = {
+  title: string;
+};
+
+const ScrollIndicator = ({ url }: ScrollIndicatorProps) => {
+  const [products, setProducts] = useState<TProducts[] | null>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [scrollPercentage, setScrollPercentage] = useState<number>(0);
+
+  const fetchData = useCallback(async () => {
+    try {
+      setLoading(true);
+      const res = await axios(url);
+      const data = res.data;
+      if (data && data.products && data.products.length > 0) {
+        setProducts(data.products);
+      } else {
+        setErrorMessage("No products found.");
+      }
+      setLoading(false);
+    } catch (e) {
+      setErrorMessage((e as Error).message);
+      setLoading(false);
+    }
+  }, [url]);
+  const handleScrollPercentage = () => {
+    const howMuchScrolled: number =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    const height: number =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    setScrollPercentage((howMuchScrolled / height) * 100);
+  };
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollPercentage);
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  });
+
+  if (loading) {
+    return <div>Loading data ! Please wait</div>;
+  }
+  if (errorMessage !== null) {
+    return <div>Error occured ! {errorMessage}</div>;
+  }
+
+  return (
+    <div>
+      <div className="top-container fixed top-0 z-10 w-full bg-[#075b0a] py-3 text-[#fff]">
+        <h2 className="text-center text-2xl">Custom Scroll Indicator</h2>
+        <div className="scroll-progress-tracking-container h-[12px] pt-4">
+          <div
+            className="current-progress-bar h-[12px] bg-[#8b02b5]"
+            style={{ width: `${scrollPercentage}%` }}
+          ></div>
+        </div>
+      </div>
+      <div className="data-container mt-5">
+        <h3 className="text-center text-3xl font-semibold">
+          List Product Test Scroll Indicator
+        </h3>
+        {products && products.length > 0
+          ? products.map((val) => (
+              <p className="mt-2 text-center">{val.title}</p>
+            ))
+          : null}
+      </div>
+    </div>
+  );
+};
+
+export default ScrollIndicator;
